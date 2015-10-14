a55dc30d166fc15a6919ae28d13b52df6b2202b5
openks <zhuyangxing@sohu.com>
2015-10-14 16:40:40 +0800

    界面使用淘宝的sui 登陆页面样式修改 登陆功能实现 注册功能实现
    用户列表页面成形
    用户详情页面成形
    
    @todo 
    1.用户列表页面的搜索功能
    2.用户列表页到用户详情页的跳转
    3.用户详情页的编辑
    
    Signed-off-by: openks <zhuyangxing@sohu.com>

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -9,13 +9,28 @@ $(function() {
 			data:{"uname":uname,"pwd":CryptoJS.SHA256(pwd).toString()},
 			async: true, //设置为同步操作就可以给全局变量赋值成功 
 			success: function(data) {
-				console.warn(data);
-				$(".result").text(data.result);
+				Zepto.alert(data.result);
 			},
 			error: function(data) {
-				console.error(data)
+				Zepto.alert(data.result);
 			}
 		});
-		console.log("clicked：" + uname + "--" + pwd);
 	})
-})
+	$("#bt-reg").on("click", function() {
+		var uname = $("#userName").val();
+		var pwd = $("#password").val();
+		$.ajax({
+			type: "post",
+			url: "regiest",
+			dataType: 'json',
+			data:{"uname":uname,"pwd":CryptoJS.SHA256(pwd).toString()},
+			async: true, //设置为同步操作就可以给全局变量赋值成功 
+			success: function(data) {
+				Zepto.alert(data.result);
+			},
+			error: function(data) {
+				Zepto.alert(data.result);
+			}
+		});
+	})
+});
--- routes/index.js
+++ routes/index.js
@@ -8,30 +8,69 @@ log4js.configure('conf/log4js_conf.json');
 var loginLogger = log4js.getLogger("login");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', {
+		title: 'Express'
+	});
 });
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: '登陆页' });
+	res.render('login', {
+		title: '登陆页'
+	});
 });
+//获取用户列表
 router.get('/users', function(req, res, next) {
-	console.log(User);
-	User.find().exec(function(err,data){
+	User.find().exec(function(err, data) {
 		res.render('showUsers', {
 			user: data,
-			title:"用户列表"
+			title: "用户列表"
 		})
-	})  
+	})
 });
+router.get('/userDetail', function(req, res, next) {
+	var user = {};
+	user.userName = "张三";
+	user.birthday = "2015-05-01";
+	user.age = 20;
+	res.render('userDetail', {
+		user: user,
+		title: user.userName+"的详细信息"
+	})
+});
+//登录
 router.all('/login2', function(req, res, next) {
-		User.find({"userName":req.body.uname,"passWord":req.body.pwd}).exec(function(err,data){
-			var result;
-			if(data.length!=0){
-				result="登陆成功！！";
-			}else{
-				result="用户名或密码错误请重试";
-			}
-			loginLogger.info("--userName:"+req.body.uname+"--"+result);
-			res.json({"result":result});
-	})  
+	User.find({
+		"userName": req.body.uname,
+		"passWord": req.body.pwd
+	}).exec(function(err, data) {
+		var result;
+		if (data.length != 0) {
+			result = "登陆成功！！";
+		} else {
+			result = "用户名或密码错误请重试";
+		}
+		loginLogger.info("--userName:" + req.body.uname + "--" + result);
+		res.json({
+			"result": result
+		});
+	})
+});
+//注册
+router.all('/regiest', function(req, res, next) {
+	var newUser = new User({
+		"userName": req.body.uname,
+		"passWord": req.body.pwd
+	});
+	newUser.save(function(err, data) {
+		loginLogger.info("--userName:" + req.body.uname + "--" + result);
+		var result;
+		if (err.code == "11000") {
+			result = "用户名已存在！";
+		} else {
+			result = "注册成功！！";
+		}
+		res.json({
+			"result": result
+		});
+	})
 });
 module.exports = router;
