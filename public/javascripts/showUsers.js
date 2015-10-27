ca65cb1bc63b8012da414eed842d91c574fd3a70
openks <zhuyangxing@sohu.com>
2015-10-27 13:52:46 +0800

    新增修改用户信息样式 Signed-off-by: openks <zhuyangxing@sohu.com>

--- public/javascripts/showUsers.js
+++ public/javascripts/showUsers.js
@@ -38,17 +38,24 @@ $(function() {
 	};
 	getAndShowUsers();
 	//用户列表页点击返回跳转到主页
-	$(".js-users-back").on("click",  function() {
-		location.href="home";
+	$(".js-users-back").on("click", function() {
+		location.href = "home";
 	});
 	//用户列表页点击用户所在行触发事件
 	$(".js-user-search-result").on("click", ".item-content", function() {
 		var id = $(this).attr("data-id");
 		var obj = getObjByProperty("users", "_id", id, "s");
+		var sex;
 		$("#detailPage .title,.js-show-uName").text(obj.userName);
+		$(".js-show-uSex").val(SEX[obj.gender]).attr("data-old", obj.gender);
+		$(".js-show-uBirthday").val(obj.birthday).attr("data-old", obj.birthday);
+		$(".js-show-uAge").val(getYears(obj.birthday));
 		$("#uname-change").val(obj.userName).attr({
 			"data-uid": id,
 			"data-old": obj.userName
+		});
+		$(".user-detail-edit").css({
+			"display": "none"
 		});
 		Zepto.router.loadPage("#detailPage");
 	});
@@ -72,6 +79,14 @@ $(function() {
 			Zepto(".js-show-uSex").triggerHandler("click");
 		}
 	});
+	$(".js-show-uSex").on("change", function() {
+		var _this = $(this);
+		_this.attr({
+			"data-change": _this.attr("data-old") != SEX[_this.val()],
+			"data-new": SEX[_this.val()]
+		});
+		editBtStatus();
+	});
 	//用户详情页点击生日行触发事件
 	$(".js-item-uBirthday").on("click", function(e) {
 		e.preventDefault();
@@ -82,8 +97,13 @@ $(function() {
 	});
 	//生日变化时计算年龄
 	$(".js-show-uBirthday").on("change", function() {
-		var year = parseInt((new Date() - new Date($(this).val())) / (365 * 24 * 60 * 60 * 1000), 10);
-		$(".js-show-uAge").val(year);
+		var _this = $(this);
+		_this.attr({
+			"data-change": _this.attr("data-old") != _this.val(),
+			"data-new": _this.val()
+		});
+		editBtStatus();
+		$(".js-show-uAge").val(getYears($(this).val()));
 	});
 	//用户名修改完成后保存
 	$("#bt-edit-uname").on("click", function() {
@@ -134,13 +154,50 @@ $(function() {
 				values: ['男', '女', '保密']
 			}]
 		});
-		$(".js-show-uBirthday").val("2010-01-01");
 		Zepto(".js-show-uBirthday").calendar({
 			maxDate: new Date(),
-			value:[$(".js-show-uBirthday").val()]
+			value: [$(".js-show-uBirthday").val()]
 		});
 	});
 	Zepto.init();
 	//设置默认值 性别默认设为女
-	Zepto(".js-show-uSex").picker("setValue", ["女"]);
-})
+	//	Zepto(".js-show-uSex").picker("setValue", ["女"]);
+});
+/**
+ * 根据年龄字符串获取年龄值
+ * @param {Object} dateStr "2010-01-01"
+ */
+function getYears(dateStr) {
+	var now = new Date();
+	var old = new Date(dateStr);
+	var years = now.getFullYear() - old.getFullYear();
+	if (now.getMonth() < old.getMonth()) {
+		years = years - 1;
+	}
+	if (now.getMonth() == old.getMonth() && now.getDate() < old.getDate()) {
+		years = years - 1;
+	}
+	years = years < 0 ? 0 : years;
+	//  console.log(dateStr  + "--" + years);
+	return years;
+}
+
+function editBtStatus() {
+	if ($("[data-change='true']").length > 0) {
+		$(".user-detail-edit").css({
+			"display": "block"
+		});
+	} else {
+		$(".user-detail-edit").css({
+			"display": "none"
+		});
+	}
+}
+
+var SEX = {};
+SEX[0] = "男";
+SEX[1] = "女";
+SEX[2] = "保密";
+SEX["男"] = 0;
+SEX["女"] = 1;
+SEX["保密"] = 2;
