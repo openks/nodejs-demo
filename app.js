390a3b2e219c5dfac5b1491ab247800a028229f9
openks-朱阳星 <openks@foxmail.com>
2016-04-19 16:16:21 +0800

    设置404页面

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ app.use(session({
 }));
 
 app.use(function (req, res, next) {
-  var user = req.session.user;
-  if (!user) {
-    user = req.session.user = {};
-  }
-  next();
+	var user = req.session.user;
+	if (!user) {
+		user = req.session.user = {};
+	}
+	next();
 })
 
 //var app = express();
@@ -51,9 +51,9 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -61,23 +61,26 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		if(err.status===404){
+			res.redirect("/404.html");
+		}
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 
